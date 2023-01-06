b5810959a37b9acde34233d8fa95682a21f8879a
elliejohnson10 <113377181+elliejohnson10@users.noreply.github.com>
2023-01-06 09:55:22 -0500

    grid!

--- CreativeCoding/Grid/sketch.js
+++ CreativeCoding/Grid/sketch.js
@@ -1,7 +1,26 @@
-function setup() {
-
+function setup() { //runs once
+createCanvas (720, 720); //creates a canvas 720x720
+background (224, 212, 255);
 }
 
-function draw() {
-
+function draw() { //runs in a loop
+fill (150, 225, 250); //fills shape w RGB color
+strokeWeight (8); //assigns stroke weight
+translate (60, 60); //creates a new origin pt at 60,60
+quad (0,0,
+      300,0,
+      300,300,
+      0,300);
+quad (300,0,
+      600,0,
+      600,300,
+      300,300); //add 300 in x direction
+quad (0,300,
+      300,300,
+      300,600,
+      0,600);//add 300 in y direction
+quad (300,300,
+      600,300,
+      600,600,
+      300,600); //add 300 in x direction
 }
