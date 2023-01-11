de7da24bda2fc39941ddbcb44b1a61f8fb537709
elliejohnson10 <113377181+elliejohnson10@users.noreply.github.com>
2023-01-11 09:18:21 -0500

    new folder

---
+++ CreativeCoding/Variables_gif copy/sketch.js
@@ -0,0 +1,24 @@
+//create a program that enables us to customize the number of squares in a grid
+let gif;
+function preload (){
+  gif = loadImage ('assets/AIBG.gif');
+}
+
+function setup() { //runs once
+createCanvas (windowWidth, windowWidth); //creates a canvas 720x720
+//background (224, 212, 255);
+}
+
+function draw() { //runs in a loop
+fill (150, 225, 250); //fills shape w RGB color
+strokeWeight (1); //assigns stroke weight
+
+var num = 2; //number of sides in the array
+var sideLen = windowWidth/num; //side length variable
+
+for (var y = 0; y < windowWidth; y= y + sideLen){
+  for (var x=0; x < windowWidth; x= x + sideLen) {
+image (gif, x, y, windowWidth/num, windowWidth/num);
+  }
+}
+}
